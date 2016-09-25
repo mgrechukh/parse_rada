a363684779c2794a10e353877b4a1b3ab23cbeaf
Mykola Grechukh <mgrechukh@satelliz.com>
2016-09-25 16:09:49 +0300

    fetch declarations and filter those who received a heritage

---
+++ fetch_declarations.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# This Python file uses the following encoding: utf-8
+
+import urllib2
+from BeautifulSoup import BeautifulSoup
+
+full_list_url = 'http://w1.c1.rada.gov.ua/pls/site2/fetch_mps?skl_id=9'
+info_page_prefix = 'http://itd.rada.gov.ua/mps/info/page'
+
+decl_page_pattern = "http://itd.rada.gov.ua/declview/Home/Main/%s/%s" # % dep_id, year
+
+def soupize(url):
+	page = urllib2.urlopen(url)
+	return BeautifulSoup(page)
+
+def read_deps(url_list):
+	soup = soupize(url_list)
+	print "-- parsing titles"
+	for p in soup.findAll(name = 'p', attrs = {'class': 'title'}):
+  		a = p.find('a')
+
+		if a.get('href').startswith(info_page_prefix):
+			dep_id = a.get('href').split('/')[-1]
+			dep_name = a.text
+			yield (dep_id, dep_name)
+		else:
+			print 'Bad link format', a.get('href'), a.text
+
+
+def read_decl(dep_id, year = 2015):
+	decl_page_url = decl_page_pattern % (dep_id, year)
+	#page = urllib2.urlopen(decl_page_url)
+	#with open("decl-%s.txt" % dep_id, 'w') as f:
+	#	f.write(page.read())
+	soup = soupize(decl_page_url)
+	heritage = soup.find('td', text = "спадщина".decode('utf-8'))
+	if heritage:
+		amounts = heritage.parent.parent.findAll('td', {'style': 'text-align: center'})
+		own = amounts[0].text.strip() or 0 # look on html
+		fam = amounts[1].text.strip() or 0
+		return (own, fam, decl_page_url)
+	else:
+		return 0, 0, decl_page_url
+
+print "-- fetching list"
+for x in read_deps(full_list_url):
+	got_legacy = read_decl(x[0])
+	if got_legacy and filter(bool, got_legacy[:2]):
+		print x[1], x[0], "own: %s, family: %s |src %s" % (got_legacy)
